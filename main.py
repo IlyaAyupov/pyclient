ec37ddd73b5b2f6e0d68ef6bebbe13a7ce0b0cdc
Ilya <ayupow450106@gmail.com>
2020-08-28 00:23:41 +0500

    integrated JSON

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import base64
 import configparser
 import curses
 import datetime
+import json
 import os
 import socket
 import threading
@@ -9,18 +11,19 @@ from zlib import compress, decompress
 
 import ui
 
-screen=None
+screen = None
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 
 indent = 0
 
+
 def register(name):
     global indent
     data = send(register_data(name))
-    if data[:2] == b'OK':
-        indent = int(data[2:].decode('utf-8'))
+    if data["status"] == "ok":
+        indent = data["id"]
     return data
 
 
@@ -32,30 +35,21 @@ def register_data(name):
     s = ""
     for i in ok:
         s += i
-    return b'\x00' + (s + name).encode('utf-8')
+    return json.dumps({"type": 0, "data": {"name": name, "key": s}})
 
 
 def send_message(sender_id, reciever_id, group, text):
     key = RSA.import_key(open('pr.Key').read())
     h = SHA256.new(text)
     signature = pss.new(key).sign(h)
-    send_data = bytearray()
-    send_data.append(1)
-    send_data.append(int(group))
-    send_data.append(sender_id >> 16)
-    send_data.append((sender_id >> 8) & 0xFF)
-    send_data.append(sender_id & 0xFF)
-    send_data.append(reciever_id >> 16)
-    send_data.append((reciever_id >> 8) & 0xFF)
-    send_data.append(reciever_id & 0xFF)
-    send_data += signature + text
-    return send_data
+    return json.dumps(
+        {"type": 1, "data": {"sender_id": sender_id, "receiver_id": reciever_id, "group": group, "text": text.decode('utf-8'),"sign":base64.b64encode(signature).decode('utf-8')}})
 
 
 def send(data):
     sock = socket.socket()
-    sock.connect(('167.172.32.117', 1337))
-    data = compress(data)
+    sock.connect(('localhost', 1337))
+    data = compress(data.encode('utf-8'))
     ou = bytearray()
     ou.append(len(data) >> 8)
     ou.append(len(data) & 0xFF)
@@ -63,20 +57,16 @@ def send(data):
     sock.send(ou)
     leng = sock.recv(2)
     leng = (leng[0] << 8) + leng[1]
-    return decompress(sock.recv(leng))
+    return json.loads(decompress(sock.recv(leng)).decode('utf-8'))
 
 
 def get_users():
-    return b'\x02'
+    return json.dumps({"type": 2})
 
 
 def get_messages():
-    out = bytearray()
-    out.append(3)
-    out.append(indent >> 16)
-    out.append((indent >> 8) & 0xFF)
-    out.append((indent) & 0xFF)
-    return out
+    return json.dumps({"type": 3, "data": {"id": indent}})
+
 
 def _start_curses():
     global screen
@@ -84,42 +74,45 @@ def _start_curses():
     curses.cbreak()
     screen.keypad(1)
 
+
 def redraw():
     screen.refresh()
     history.redraw()
     title.redraw()
     prompt.redraw()
+
+
 def updater():
+    history.messages=[]
     while True:
-        messes = send(get_messages()).decode('utf-8')
-        if messes.count(':') > 0:
-            messes = messes.split('\n')
+        messes = send(get_messages())["data"]
+        for i in messes:
+            recid = i[0]
+            text = i[1]
+            try:
+                usr = users_dict[recid]
+            except KeyError:
+                usr = "UNK"
+            msg = ui.Message(datetime.datetime.now(), usr, text)
+            history.append(msg)
 
-            for i in messes:
-                recid = i[:i.index(':')]
-                text = i[i.index(':') + 1:]
-                try:
-                    usr = users_dict[int(recid)]
-                except KeyError:
-                    usr = "UNK"
-                msg = ui.Message(datetime.datetime.now(), usr, text)
-
-                history.append(msg)
         history.redraw()
-        sleep(3)
+        sleep(1)
+
+
 _start_curses()
 layout = ui.Layout()
-title   = ui.Title(layout, screen)
+title = ui.Title(layout, screen)
 history = ui.History(layout, screen)
-prompt  = ui.Prompt(layout, screen)
+prompt = ui.Prompt(layout, screen)
 
 redraw()
 
 if not (os.path.exists("config.cfg") and os.path.exists("op.Key") and os.path.exists("pr.Key")):
-    history.append(ui.Message(datetime.datetime.now(),"Введи имя",""))
+    history.append(ui.Message(datetime.datetime.now(), "Введи имя", ""))
     history.redraw()
     nam = prompt.getstr().decode('utf-8')
-    history.messages=[]
+    history.messages = []
     history.redraw()
     config = configparser.ConfigParser()
     config.add_section("Chat")
@@ -135,38 +128,30 @@ if (os.path.exists("config.cfg") and os.path.exists("op.Key") and os.path.exists
     name = config["Chat"]["username"]
     indent = int(config["Chat"]["id"])
 
-
-
-
-
-    history.messages=[]
-    users = send(get_users()).decode('utf-8').strip()
-    users_dict = {}
-    for i in users.split('\n'):
-        users_dict.update({int(i.strip().split(':')[0]): i.strip().split(':')[1]})
+    history.messages = []
+    users_dict = send(get_users())["users"]
     for i in users_dict:
-        history.append(ui.Message(datetime.datetime.now(),"ВЫБИРАЙ",f"{i}: {users_dict[i]}"))
+        history.append(ui.Message(datetime.datetime.now(), "ВЫБИРАЙ", f"{i}: {users_dict[i]}"))
         history.redraw()
     try:
-        to=int(prompt.getstr().decode('utf-8'))
+        to = int(prompt.getstr().decode('utf-8'))
     except Exception:
         to = int(prompt.getstr().decode('utf-8'))
-    history.messages=[]
+    history.messages = []
     history.redraw()
     t = threading.Thread(target=updater)
     t.start()
 while True:
-        redraw()
-        text = prompt.getstr()
-        if text == b'':
-            continue
-        if text == b'/quit':
-            break
-        send(send_message(indent, to, False, text))
-        now = datetime.datetime.now()
-        msg = ui.Message(now, "ME", text)
-        history.append(msg)
+    redraw()
+    text = prompt.getstr()
+    if text == b'':
+        continue
+    if text == b'/quit':
+        break
+    send(send_message(indent, to, False, text))
+    now = datetime.datetime.now()
+    msg = ui.Message(now, "ME", text)
+    history.append(msg)
 
-        history.redraw()
-        prompt.reset()
-
+    history.redraw()
+    prompt.reset()
